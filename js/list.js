63ea50f4d8e734038a60ac60178aa15a4e6004a0
Petteri Hellman <petteri.hellman@gmail.com>
2015-11-26 12:09:24 +0200

    List code

---
+++ js/list.js
@@ -0,0 +1,15 @@
+//List JS tiedosto
+
+//function Person(firstName, lastName) {
+	//this.firstName = firstName;
+	//this.lastName = lastName;
+//}
+
+//var addPerson = new Person()
+
+//for(var i = 0; i < Person.length; i ++) {
+//	console.log(Person[i])
+//}
+
+//Listener
+	
